8a86f8bbe2e8b3aa80801d8ab625d3ed06c443b1
scottjeremy401-creator <scottjeremy401@gmail.com>
2026-04-10 11:18:07 +0300

    add app

---
+++ app/src/main/java/com/mitch/myapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.mitch.myapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mitch.myapp.ui.login.LoginScreen
+import com.mitch.myapp.ui.theme.MyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()){ innerPadding ->
+                   LoginScreen(modifier = Modifier.padding(innerPadding))
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyAppTheme {
+
+    }
+}}
---
+++ app/src/main/java/com/mitch/myapp/ui/login/login.kt
@@ -0,0 +1,103 @@
+package com.mitch.myapp.ui.login
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieCompositionSpec.RawRes
+import com.airbnb.lottie.compose.LottieConstants
+
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.mitch.myapp.R
+
+
+lateinit var placeholder: () -> Unit
+
+@Composable
+fun LoginScreen(modifier: Modifier){
+    //textInput
+    var emailInput by remember {mutableStateOf(value = TextFieldValue(""))  }
+    var passwordInput by remember {mutableStateOf(value = TextFieldValue(""))  }
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+    ){
+        //lottie animation
+        LottieAnimationWidget()
+        //email input
+        OutlinedTextField(
+            value = emailInput,
+            onValueChange = { emailInput=it},
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Email,
+                    contentDescription = "Email"
+
+                )},
+                placeholder = {
+                    Text(text = "eg email@gmail.com")
+
+            },
+            maxLines = 1,
+            modifier = Modifier.fillMaxWidth()
+
+        )
+
+        //password input
+        OutlinedTextField(
+            value = passwordInput,
+            onValueChange = { passwordInput=it},
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Email,
+                    contentDescription ="Email"
+
+                )},
+                placeholder = {
+                    Text(text="eg 1234")
+
+            },
+            maxLines = 1,
+            modifier = Modifier.fillMaxWidth()
+
+        //button
+
+
+
+    }
+    }
+
+
+@Composable
+fun LottieAnimationWidget(){
+    val composition by rememberLottieComposition(
+        spec = RawRes( R.raw.login))
+    val progress by animateLottieCompositionAsState(
+        composition,
+    iterations = LottieConstants.IterateForever
+    )
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+        modifier = Modifier.size(350.dp)
+    )
+}
